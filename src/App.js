8ef77d15d90407649db06ce7e1e2965f9111b56b
juan.goez <juan.goez@enlared.net.co>
2019-11-05 17:27:53 -0500

    recto8

--- src/App.js
+++ src/App.js
@@ -3,22 +3,49 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      tasks: ['Sacar la ropa','Hacer la cama', 'Leer un rato'],
+      inputTask: ''
+    }
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            { this.state.tasks.map((task) => 
+              <li key={task}>{task}</li>
+            )}
           </ul>
            <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+             <input value={this.state.inputTask} onKeyPress={this.onKeyPressTask} onChange={this.onChangeTask} type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
            </form>
         </div>
       </div>
     )
+  }
+
+  onChangeTask = (e) => {
+    this.setState({
+      inputTask: e.target.value
+    });
+  }
+
+  onKeyPressTask = (e) => {
+    if (e.key === 'Enter'){
+      e.preventDefault();
+      this.setState({
+        tasks: this.state.tasks.concat(this.state.inputTask)
+      });
+      this.setState({
+        inputTask: ''
+      });
+    }
   }
 }
 
